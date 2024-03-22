dd6ef93955a3f4824af7f71d2ab243abd40491d3
Aditya Pratap Singh <2004adityaprataps@gmail.com>
2024-03-22 17:53:31 +0530

    Suspense added

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,70 +1,13 @@
-'use client'
-import Form from "@components/Form"
-import { useEffect, useState } from "react"
-import { useRouter,useSearchParams } from "next/navigation"
+import EditPrompt from '@components/EditPrompt';
+import { Suspense } from 'react';
+ 
 
-const EditPrompt = () => {
-    const searchParams=useSearchParams();
-    const promptId=searchParams.get('id');
-    const router=useRouter();
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: '',
-    })
+const UpdatePromptPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPrompt />
+    </Suspense>
+  );
+};
 
-    const updatePrompt = async (e) => {
-        e.preventDefault();
-        setSubmitting(true);
-       
-
-        if(!promptId) return alter('Promt ID not fount')
-
-        try {
-            const response = await fetch(`/api/prompt/${promptId}`, {
-                method: 'PATCH',
-                body: JSON.stringify({
-                    prompt: post.prompt,
-                    tag: post.tag
-                })
-            })
-
-            if (response.ok) {
-                router.push('/');
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-        finally {
-            setSubmitting(false)
-        }
-    }
-
-    useEffect(()=>{
-        const getPromptDetails=async()=>{
-            const response=await fetch(`/api/prompt/${promptId}`)
-            const data=await response.json();
-
-            setPost({
-                prompt:data.prompt,
-                tag:data.tag,
-            })
-        }
-
-        if(promptId) getPromptDetails();
-
-    },[promptId])
-    return (
-
-        <Form
-            type="Edit"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-        />
-    )
-}
-
-export default EditPrompt
+export default UpdatePromptPage;
